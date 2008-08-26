1f2236ce7d5519215f6c7961775f0a0aa3ffe426
jonasharty <jonasharty@ff1743de-6255-0410-9ce6-7d606ac22720>
2008-08-26 20:52:04 +0000

    Código fonte do Learning

---
+++ trunk/Learning/Debug/Sender/Sender.d
@@ -0,0 +1,11 @@
+Sender/Sender.d: ../Sender/Sender.cpp ../Sender/Sender.h \
+  ../Sender/../Almtf.h ../Sender/../Utils/Utils.h \
+  ../Sender/../Utils/Socket_Utils.h
+
+../Sender/Sender.h:
+
+../Sender/../Almtf.h:
+
+../Sender/../Utils/Utils.h:
+
+../Sender/../Utils/Socket_Utils.h:
---
+++ trunk/Learning/Debug/Utils/Socket_Utils.d
@@ -0,0 +1,3 @@
+Utils/Socket_Utils.d: ../Utils/Socket_Utils.cpp ../Utils/Socket_Utils.h
+
+../Utils/Socket_Utils.h:
